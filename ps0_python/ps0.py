f3f4d9a5d574fe8b580dbaef44a5a00d10eccd47
Aaron96 <love-360@qq.com>
2020-01-17 09:47:58 +1030

    update question4

--- ps0_python/ps0.py
+++ ps0_python/ps0.py
@@ -46,3 +46,37 @@ img1_mono2[img2_center[0]-50:img2_center[0]+50,
                       img1_center[1]-50:img1_center[1]+50]
 
 cv2.imwrite('output/ps0-3-a-1.png', img1_mono2)
+
+#Question 4: Arithmetic and Geometric operations
+'''
+    a. Min, max, mean, standard value of img1_green
+    b.normalise img1_green and then multiply with 10 and add the mean
+    c. shift img by 2 pixels to the left
+    d.subtract shifted version from original
+'''
+
+#4a
+min1 = img1_green.min()
+max1 = img1_green.max()
+mean1 = img1_green.mean()
+std1 = img1_green.std()
+print('4a) img1_green: min=%d, max=%d, mean=%f std=%f' %(min1,max1,mean1,std1))
+
+#4b
+img1_green_normed = img1_green.copy()
+img1_green_normed = cv2.add(cv2.multiply(cv2.divide(cv2.subtract(img1_green_normed, mean1), std1), 10), mean1)
+cv2.imwrite('output/ps0-4-b-1.png', img1_green_normed)
+
+#4c
+img1_shifted = img1_green.copy()
+rows,cols = img1_shifted.shape[0:2]
+M = np.float32([[1,0,-2],[0,1,0]])
+img1_shifted = cv2.warpAffine(img1_shifted, M, (cols,rows))
+cv2.imwrite('output/ps0-4-c-1.png', img1_shifted)
+
+#4d
+img1_diff_shifted = img1_shifted.copy()
+img1_diff_shifted = img1_diff_shifted-img1_green
+cv2.imwrite('output/ps0-4-d-1.png', img1_diff_shifted)
+
+#Question 5: Noise
